7aa73dd78c699b3420eefe8a1e815594415ea063
hojunkim13 <hojunkim13@gmail.com>
2021-12-14 18:00:26 +0900

    Modify loss plot function

--- Utils.py
+++ Utils.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 
 
-def plotLoss(losses_dict, path):
+def plotLoss(losses_dict, path, info=["IC", "BC", "PDE"]):
     fig, axes = plt.subplots(1, 3, sharex=True, sharey=True, figsize=(10, 6))
     axes[0].set_yscale("log")
-    for i, j in zip(range(3), ["IC", "BC", "PDE"]):
+    for i, j in zip(range(3), info):
         axes[i].plot(losses_dict[j.lower()])
         axes[i].set_title(j)
     plt.show()
