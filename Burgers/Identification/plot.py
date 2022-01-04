da324070fe07e1ae68dc9be394595b0454013c0c
hojunkim13 <hojunkim13@gmail.com>
2022-01-04 11:49:32 +0900

    Modify plot style

--- Burgers/Identification/plot.py
+++ Burgers/Identification/plot.py
@@ -45,9 +45,9 @@ with torch.no_grad():
     u_pred = pinn.net(torch.hstack((x_ts, t_ts))).cpu().numpy().reshape((100, 256)).T
 
 ################ Plot ###################
-fig = plt.figure(figsize=(16, 9.5))
+fig = plt.figure(figsize=(8, 6))
 gs0 = GridSpec(1, 2, figure=fig)
-gs0.update(top=0.94, bottom=0.70, left=0.15, right=0.85, wspace=0)
+gs0.update(top=0.94, bottom=0.70, left=0.20, right=0.80, wspace=0)
 
 ax = fig.add_subplot(gs0[0, :])
 im = ax.imshow(
@@ -64,7 +64,7 @@ data_pt = ax.plot(
     x_train,
     "kx",
     label=f"Data ({len(t_train)}) points",
-    markersize=4,
+    markersize=3,
     clip_on=False,
     alpha=0.8,
 )
@@ -72,14 +72,14 @@ data_pt = ax.plot(
 divider = make_axes_locatable(ax)
 cax = divider.append_axes("right", size="3%", pad=0.2)
 cbar = fig.colorbar(im, cax=cax, label="$u(x,t)$")
-cbar.ax.tick_params(labelsize=15)
+
 
 ax.legend(
     loc="upper center",
-    bbox_to_anchor=(0.9, -0.05),
+    bbox_to_anchor=(0.92, -0.1),
     ncol=5,
     frameon=False,
-    prop={"size": 15},
+    prop={"size": 10},
 )
 ax.set_xlabel("$t$")
 ax.set_ylabel("$x$")
@@ -92,17 +92,17 @@ t_slice = [0.25, 0.5, 0.75]
 axes = []
 for i in range(3):
     ax = fig.add_subplot(gs1[0, i])
-    ax.plot(x, u[:, int(t_slice[i] * 100)], "b-", linewidth=2, label="Exact")
-    ax.plot(x, u_pred[:, int(t_slice[i] * 100)], "r--", linewidth=2, label="Prediction")
+    ax.plot(x, u[:, int(t_slice[i] * 100)], "b-", linewidth=3, label="Exact")
+    ax.plot(x, u_pred[:, int(t_slice[i] * 100)], "r--", linewidth=3, label="Prediction")
     ax.set_xlabel("$x$")
     ax.set_ylabel("$u(x,t)$")
-    ax.set_title(f"$t = {t_slice[i]}s$", fontsize=10)
+    ax.set_title(f"$t = {t_slice[i]}s$", fontsize=12)
     ax.axis("square")
     ax.set_xlim([-1.1, 1.1])
     ax.set_ylim([-1.1, 1.1])
     axes.append(ax)
 axes[1].legend(
-    loc="upper center", bbox_to_anchor=(0.5, -0.15), ncol=2, frameon=False, fontsize=10
+    loc="upper center", bbox_to_anchor=(0.5, -0.18), ncol=2, frameon=False, fontsize=10
 )
 
 ##############################################
@@ -118,13 +118,13 @@ pinn.net.load_state_dict(torch.load("./Burgers/identification/weight_noisy1.pt")
 l1_noisy = pinn.net.state_dict()["lambda_1"].cpu().item()
 l2_noisy = np.exp(pinn.net.state_dict()["lambda_2"].cpu().item())
 
-s1 = r"$\begin{tabular}{ |c|c| }  \hline Correct PDE & $u_t + u u_x - 0.0031831 u_{xx} = 0$ \\  \hline Identified PDE (clean data) & "
-s2 = r"$u_t + %.5f u u_x - %.7f u_{xx} = 0$ \\  \hline " % (l1, l2)
-s3 = r"Identified PDE (1\% noise) & "
-s4 = r"$u_t + %.5f u u_x - %.7f u_{xx} = 0$  \\  \hline " % (l1_noisy, l2_noisy)
-s5 = r"\end{tabular}$"
-s = s1 + s2 + s3 + s4 + s5
-ax.text(0.21, 0.35, s, size=25)
+s = r"$\begin{tabular}{ |c|c| }  \hline Correct PDE & $u_t + u u_x - 0.0031831 u_{xx} = 0$ \\  \hline Identified PDE (clean data) & "
+s += r"$u_t + %.5f u u_x - %.7f u_{xx} = 0$ \\  \hline " % (l1, l2)
+s += r"Identified PDE (1\% noise) & "
+s += r"$u_t + %.5f u u_x - %.7f u_{xx} = 0$  \\  \hline " % (l1_noisy, l2_noisy)
+s += r"\end{tabular}$"
+ax.text(0.13, 0.40, s, size=15)
+
 fig.savefig(
     "./Burgers/identification/solution.png", bbox_inches="tight", pad_inches=0, dpi=500,
 )
