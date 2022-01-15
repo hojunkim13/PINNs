fe0148d281719090c8665fbc3c60eb13b598ad94
hojunkim13 <hojunkim13@gmail.com>
2022-01-15 22:22:08 +0900

    Correct wrong boundary condition

--- Heat/main.py
+++ Heat/main.py
@@ -4,7 +4,6 @@ sys.path.append(".")
 
 from network import DNN
 import torch
-import torch.nn as nn
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import offsetbox
@@ -13,11 +12,12 @@ from matplotlib.animation import FuncAnimation
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-N_u = 100
+N_0 = 200
+N_bc = 400
 N_f = 10000
 
 x_min = 0.0
-x_max = 1.0
+x_max = 2.0
 t_min = 0.0
 t_max = 1.0
 
@@ -25,47 +25,51 @@ ub = np.array([x_max, t_max])
 lb = np.array([x_min, t_min])
 
 # initial condition, u(x, 0) = f(x)
-x_ic = np.random.uniform(low=x_min, high=x_max, size=(N_u, 1))
-t_ic = np.zeros((N_u, 1))
-u_ic = np.ones((N_u, 1))
 
-# Case 1. Const End, u(0, t) = T
-# Case 2. Insulated End, u_x(0, t) = 0
-# Case 3. Radiating End, u_x(0, t) = A * u(x0, t)
+xt_0 = np.random.uniform([x_min, 0], [x_max, 0], size=(N_0, 2))
+u_0 = np.ones_like(xt_0[:, 0:1]) * 5
 
-x_bc = np.zeros((N_u, 1))
-t_bc = np.random.uniform(low=t_min, high=t_max, size=(N_u, 1))
-u_bc = np.zeros((N_u, 1))
 
-x_u = np.concatenate((x_ic, x_bc), axis=0)
-t_u = np.concatenate((t_ic, t_bc), axis=0)
-xt_u = np.concatenate((x_u, t_u), axis=1)
-u_u = np.concatenate((u_ic, u_bc), axis=0)
+# Boundary Condition
+# u(0, t) = 10 & u(L, t) = 0 for t > 0
 
-x_f = np.random.uniform(x_min, x_max, (N_f, 1))
-t_f = np.random.uniform(t_min, t_max, (N_f, 1))
-xt_f = np.hstack([x_f, t_f])
-xt_f = np.vstack([xt_f, xt_u])
+xt_bc_0 = np.random.uniform([x_min, t_min], [x_min, t_max], size=(N_bc // 2, 2))
+u_bc_0 = np.ones((len(xt_bc_0), 1)) * 10
 
-xt_u = torch.tensor(xt_u, dtype=torch.float32).to(device)
-u_u = torch.tensor(u_u, dtype=torch.float32).to(device)
+xt_bc_1 = np.random.uniform([x_max, t_min], [x_max, t_max], size=(N_bc // 2, 2))
+u_bc_1 = np.zeros((len(xt_bc_1), 1))
+
+xt_bc = np.vstack([xt_bc_0, xt_bc_1])
+u_bc = np.vstack([u_bc_0, u_bc_1])
+
+xt_f = np.random.uniform(lb, ub, (N_f, 2))
+xt_f = np.vstack([xt_0, xt_bc, xt_f])
+
+# Convert to Tensor
+xt_0 = torch.tensor(xt_0, dtype=torch.float32).to(device)
+u_0 = torch.tensor(u_0, dtype=torch.float32).to(device)
+
+xt_bc = torch.tensor(xt_bc, dtype=torch.float32).to(device)
+u_bc = torch.tensor(u_bc, dtype=torch.float32).to(device)
+
 xt_f = torch.tensor(xt_f, dtype=torch.float32).to(device)
 
 
 class PINN:
-    c = 1
+    c = 0.8
 
     def __init__(self) -> None:
-        self.net = DNN(dim_in=2, dim_out=1, n_layer=4, n_node=20, ub=ub, lb=lb).to(
+        self.net = DNN(dim_in=2, dim_out=1, n_layer=7, n_node=40, ub=ub, lb=lb).to(
             device
         )
-        self.optimizer = torch.optim.LBFGS(
+        self.lbfgs = torch.optim.LBFGS(
             self.net.parameters(),
             lr=1.0,
             history_size=50,
             max_eval=10000,
             max_iter=10000,
         )
+        self.adam = torch.optim.Adam(self.net.parameters())
         self.iter = 0
 
     def f(self, xt):
@@ -81,74 +85,84 @@ class PINN:
         return f
 
     def closure(self):
-        self.optimizer.zero_grad()
-        u_pred = self.net(xt_u)
-        mse_u = torch.mean(torch.square(u_pred - u_u))
+        self.lbfgs.zero_grad()
+        self.adam.zero_grad()
+
+        u0_pred = self.net(xt_0)
+        mse_0 = torch.mean(torch.square(u0_pred - u_0))
+        u_bc_pred = self.net(xt_bc)
+        mse_bc = torch.mean(torch.square(u_bc_pred - u_bc))
 
         f_pred = self.f(xt_f)
         mse_f = torch.mean(torch.square(f_pred))
-        loss = mse_u + mse_f
+        loss = mse_0 + mse_bc + mse_f
         loss.backward()
         self.iter += 1
-        print(f"\r{self.iter} loss : {loss.item():.4e}", end="")
+        print(
+            f"\r{self.iter}, Loss : {loss.item():.5e}, ic : {mse_0:.3e}, bc : {mse_bc:.3e}, f : {mse_f:.3e}",
+            end="",
+        )
+
         if self.iter % 500 == 0:
             print("")
         return loss
 
 
 pinn = PINN()
-pinn.optimizer.step(pinn.closure)
+for i in range(5000):
+    pinn.closure()
+    pinn.adam.step()
+pinn.lbfgs.step(pinn.closure)
 
 ####################### Plot ########################
-
 x = np.arange(x_min, x_max, 0.01)
 t = np.arange(t_min, t_max, 0.01)
-ms_x, ms_t = np.meshgrid(x, t)
 
-x = ms_x.reshape(-1, 1)
-t = ms_t.reshape(-1, 1)
+X, T = np.meshgrid(x, t)
 
-x = torch.tensor(x, dtype=torch.float32).to(device)
-t = torch.tensor(t, dtype=torch.float32).to(device)
-xt = torch.hstack([x, t])
-u = pinn.net(xt)
-u = u.detach().cpu().numpy()
-u = u.reshape(ms_t.shape).T
+X = X.reshape(-1, 1)
+T = T.reshape(-1, 1)
+xt_f = np.hstack([X, T])
+xt_f = torch.tensor(xt_f, dtype=torch.float32).to(device)
 
+u_pred = pinn.net(xt_f)
+u_pred = u_pred.detach().cpu().numpy()
+
+u_pred = u_pred.reshape(len(x), len(t), order="F")
 
 fig, axes = plt.subplots(2, 1, figsize=(6, 10), sharex=True)
-bar_x = np.linspace(x_min, x_max, u.shape[0])
+bar_x = np.linspace(x_min, x_max, u_pred.shape[0])
 bar_y = np.zeros_like(bar_x)
 axes[0].set_title("$1D\ Heat\ Equation$")
 axes[0].set_yticks([])
 
 im0 = axes[0].scatter(
-    bar_x,
-    bar_y,
-    c=u[:, 0],
+    x,
+    np.zeros_like(x),
+    c=u_pred[:, 0],
     marker="s",
     cmap="rainbow",
     lw=0,
-    vmin=u.min(),
-    vmax=u.max(),
+    vmin=u_pred.min() * 1.1,
+    vmax=u_pred.max() * 1.1,
 )
 fig.colorbar(im0, ax=axes[0], fraction=0.046, pad=0.04, extend="neither")
 textbox = offsetbox.AnchoredText("", loc=1)
 axes[0].add_artist(textbox)
 
 (im1,) = axes[1].plot([], [], color="k")
-axes[1].set_ylim(u.min() - u.max(), u.max())
+axes[1].set_ylim(u_pred.min() * 1.3, u_pred.max() * 1.3)
 axes[1].set_xlabel("$x$")
 axes[1].set_ylabel("$Temperature\ u(x,t)$")
 
 
 def update(frame):
-    temp = u[:, frame]
+    temp = u_pred[:, frame]
     im0.set_array(temp)
     im1.set_data(bar_x, temp)
-    textbox = offsetbox.AnchoredText(f"{ms_t[frame][0]:.2f} sec", loc=1)
+    textbox = offsetbox.AnchoredText(f"{t[frame]:.2f} sec", loc=1)
     axes[0].add_artist(textbox)
 
 
-ani = FuncAnimation(fig, update, frames=range(len(ms_t)), interval=50)
-ani.save("./Heat/solution.gif", dpi=300)
+ani = FuncAnimation(fig, update, frames=len(t), interval=50)
+ani.save("./Heat/solution.gif", dpi=100)
